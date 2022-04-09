a794dc35e14bd869149a78b607b9e5e014d4aeb7
Bigljuse <lahovWork@mail.ru>
2022-04-09 11:29:43 +0300

    Добавлен собственный конвертер под Jobject

---
+++ CSGOHUD/JObjectConverter.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSGOHUD
+{
+    public static class JObjectConverter
+    {
+        public static T ConvertToType<T>(this JObject jObject, in T type) where T : class
+        {
+            PropertyInfo[] properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (PropertyInfo property in properties)
+            {
+                if (property.PropertyType.Name.Contains("int", StringComparison.CurrentCultureIgnoreCase) == false &&
+                    property.PropertyType.Name.Contains("bool", StringComparison.CurrentCultureIgnoreCase) == false &&
+                    property.PropertyType.Name.Contains("string", StringComparison.CurrentCultureIgnoreCase) == false)
+                    continue;
+
+                JProperty? jProperty = jObject.Property(property.Name, StringComparison.OrdinalIgnoreCase);
+
+                if (jProperty is null)
+                    continue;
+
+                string jPropertyValue = jProperty.Value.ToString();
+
+                object? setValue = Convert.ChangeType(jPropertyValue, property.PropertyType);
+
+                property.SetValue(type, setValue);
+            }
+
+            return type;
+        }
+    }
+}
